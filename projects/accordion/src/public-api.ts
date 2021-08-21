b13bd8c69d6fb55b3a1e1ad7555d239b4b81de12
pierrick <pierrick.meignant@gmail.com>
2021-08-21 11:47:47 +0200

    add accordion library

---
+++ projects/accordion/src/public-api.ts
@@ -0,0 +1,4 @@
+export * from './lib/components/public-api.components';
+export * from './lib/models/public-api.models';
+export * from './lib/events/public-api.events';
+export * from './lib/accordion.module';
